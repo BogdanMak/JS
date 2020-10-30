a53d5f229668e2787fb2e39037520483395fe6db
BogdanMak <bogdan.mak20@gmail.com>
2020-10-30 13:25:35 +0200

    lesson1 HW1

--- Lesson1/lesson1.js
+++ Lesson1/lesson1.js
@@ -1,108 +1,108 @@
-
-let hello = "hello";
-let owu = "owu";
-let com = "com";
-let ua = "ua";
-let num1 = 1;
-let num2 = 10;
-let num3 = -999;
-let num4 = 123;
-let num5 = 3.14;
-let num6 = 2.7;
-let num7 = 16;
-let isTrue = true;
-let isFalse = false;
-console.log(hello);
-console.log(owu);
-console.log(com);
-console.log(ua);
-console.log(num1);
-console.log(num2);
-console.log(num3);
-console.log(num4);
-console.log(num5);
-console.log(num6);
-console.log(num7);
-console.log(isTrue);
-console.log(isFalse);
-alert(hello);
-alert(owu);
-alert(com);
-alert(ua);
-alert(num1);
-alert(num2);
-alert(num3);
-alert(num4);
-alert(num5);
-alert(num6);
-alert(num7);
-alert(isTrue);
-alert(isFalse);
-document.writeln(hello);
-document.writeln(owu);
-document.writeln(com);
-document.writeln(ua);
-document.writeln(num1);
-document.writeln(num2);
-document.writeln(num3);
-document.writeln(num4);
-document.writeln(num5);
-document.writeln(num6);
-document.writeln(num7);
-document.writeln(isTrue);
-document.writeln(isFalse);
-hello = "Hi";
-owu = "ok10";
-com = "dot";
-ua = "uk";
-num1 = 5;
-num2 = 50;
-num3 = 999;
-num4 = 321;
-num5 = 3;
-num6 = 2;
-num7 = 32;
-isTrue = false;
-isFalse = true;
-console.log(hello);
-console.log(owu);
-console.log(com);
-console.log(ua);
-console.log(num1);
-console.log(num2);
-console.log(num3);
-console.log(num4);
-console.log(num5);
-console.log(num6);
-console.log(num7);
-console.log(isTrue);
-console.log(isFalse);
-alert(hello);
-alert(owu);
-alert(com);
-alert(ua);
-alert(num1);
-alert(num2);
-alert(num3);
-alert(num4);
-alert(num5);
-alert(num6);
-alert(num7);
-alert(isTrue);
-alert(isFalse);
-document.writeln(hello);
-document.writeln(owu);
-document.writeln(com);
-document.writeln(ua);
-document.writeln(num1);
-document.writeln(num2);
-document.writeln(num3);
-document.writeln(num4);
-document.writeln(num5);
-document.writeln(num6);
-document.writeln(num7);
-document.writeln(isTrue);
-document.writeln(isFalse);
+// Task1-2!!!
+// let hello = "hello";
+// let owu = "owu";
+// let com = "com";
+// let ua = "ua";
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+// let isTrue = true;
+// let isFalse = false;
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+// console.log(isTrue);
+// console.log(isFalse);
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+// alert(isTrue);
+// alert(isFalse);
+// document.writeln(hello);
+// document.writeln(owu);
+// document.writeln(com);
+// document.writeln(ua);
+// document.writeln(num1);
+// document.writeln(num2);
+// document.writeln(num3);
+// document.writeln(num4);
+// document.writeln(num5);
+// document.writeln(num6);
+// document.writeln(num7);
+// document.writeln(isTrue);
+// document.writeln(isFalse);
+// hello = "Hi";
+// owu = "ok10";
+// com = "dot";
+// ua = "uk";
+// num1 = 5;
+// num2 = 50;
+// num3 = 999;
+// num4 = 321;
+// num5 = 3;
+// num6 = 2;
+// num7 = 32;
+// isTrue = false;
+// isFalse = true;
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+// console.log(isTrue);
+// console.log(isFalse);
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+// alert(isTrue);
+// alert(isFalse);
+// document.writeln(hello);
+// document.writeln(owu);
+// document.writeln(com);
+// document.writeln(ua);
+// document.writeln(num1);
+// document.writeln(num2);
+// document.writeln(num3);
+// document.writeln(num4);
+// document.writeln(num5);
+// document.writeln(num6);
+// document.writeln(num7);
+// document.writeln(isTrue);
+// document.writeln(isFalse);
 
 // Task3!!!
 // let name = "Jack";
@@ -262,5 +262,196 @@ document.writeln(isFalse);
 // ? console.log(!!(3/'owu'));
 // ? console.log((111 > 11 || 45 < 111) ||  !!'0');
 // ? console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+
+// Class1:
+// let str = "Привет";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+// document.writeln (typeof str);
+// document.writeln(typeof num);
+// document.writeln(typeof flag);
+// document.writeln(typeof txt);
+
+// Class2!!!
+// let a1 = 5+3;
+// let a2 = 5-3;
+// let a3 = 5*3;
+// let a4 = 5/3;
+// let a5 = 5%3;
+// document.writeln(a1);
+// document.writeln(a2);
+// document.writeln(a3);
+// document.writeln(a4);
+// document.writeln(a5);
+
+//Class3!!!
+// let a6 = 5%3;
+// let a7 = 3%5;
+// let a8 = 5+3;
+// let a9 = "5" -3;
+// let a10 = 75 + "кг";
+// document.writeln(a6);
+// document.writeln(a7);
+// document.writeln(a8);
+// document.writeln(a9);
+// document.writeln(a10);
+
+//Class4!!!
+// let height = 23;
+// let width = 10;
+// let s = height*width;
+// document.writeln(s + "cm");
+
+//Class5!!!
+// let heightC = 10;
+// let dC = 4;
+// let P = 3.14;
+// let V = P * Math.pow(dC,2) * heightC;
+// document.writeln(V + "m");
+
+// Classs6!!!
+// let a = 3;
+// let b = 4;
+// let c = Math.sqrt(Math.pow(3,2) + Math.pow(4,2));
+// document.write(c);
+
+//Class7!!!
+// let str = "Hello world";
+// document.writeln(str);
+// alert (str);
+// console.log (str);
+
+//Class8!!!
+// let name = prompt("Can you tell me your name?");
+// let age = prompt("Can you tell me your age, please?");
+// let hobby = prompt ("Can you tell me your hobby, please?");
+// document.writeln("Your name:" + name + "</br>" + "Age:" + age + "</br>" + "Hobby:" + hobby);
+
+
+// Class9!!!
+// let str1 = "Кто";
+// let str2 = "ти";
+// let str3 = "такой?";
+// document.write (str1.concat(" ", str2, " ", str3));
+
+//Class 10!!!
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); - 205 просто як строка
+// document.write(str - a + "<br/>"); - 15виконає дію віднімання
+// document.write(str * "2" + "<br/>"); - 40 виконає дію множення
+// document.write(str / 2 + "<br/>"); - 10 - виконає дію ділення
+
+//Class11!!!
+// (parseInt("3.14")) - 3
+// (parseInt("-7.875")) - -7
+// (parseInt("435")) - 435
+// (parseInt("Вася")) - Nan
+
+
+//Class12!!!
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+//Class13!!!
+// let num1 = prompt ("Can you tell your first number, please");
+// let num2 = prompt ("Can you tell me your second number, please");
+// let result = num1 + num2;
+// alert(result);
+// 14. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию
+// и возраст, а вам не обходимо вывести строку такого вида
+// Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+
+//Class14!!!
+// let name = prompt ("Can you tell me your name, please?");
+// let surName = prompt("Can you tel me your Surname, please?");
+// let age = prompt ("Can you tell me your age, please?");
+// alert ("Доброго вечера " + name + " " + surName + "," + " мои поздравления что вам " + age);
+//
+// let num1 = prompt("Can you tell me first number, please?");
+// let num2 = prompt("Can you tell me second number, please?");
+// let num3 = prompt("Can you tell me third number, please?");
+//
+// if (num1<num2) {
+//     if (num2<num3){
+//         document.write("Your numbers are in ascending order: " + num1 + "," + num2 + "," + num3);
+//     }
+//     else if (num2>num3){
+//         if (num3>num1){
+//             document.write("Your numbers are in ascending order: " + num1 + "," + num3 + "," + num2);
+//         }
+//         else {
+//             document.write("Your numbers are in ascending order: " + num3 + "," + num1 + "," + num2);
+//         }
+//     }
+// }
+// if (num1>num2) {
+//     if (num3>num1){
+//     document.write("Your numbers are in ascending order: " + num2 + "," + num1 + "," + num3);
+//     }
+//     else if (num3<num1){
+//         if (num3>num2) {
+//             document.write("Your numbers are in ascending order: " + num2 + "," + num3 + "," + num1);
+//         }
+//         else {
+//             document.write("Your numbers are in ascending order: " + num3 + "," + num2 + "," + num1);
+//         }
+//     }
+// }
+
+// Class15!!!
+// let light = prompt("Tell me color of the traffic lights!!!")
+// switch (light) {
+//     case "green" :
+//         document.write("GO!");
+//         break;
+//     case "yellow":
+//         document.write("WAIT!!!");
+//         break;
+//     case "red":
+//         document.write("STOP!!!");
+//         break;
+//     case "broked":
+//         document.write ("You can do whatever you want!");
+// }
+
+// Classe16!!!
+let isRoadClear = confirm("Tell me, please is road clear?");
+let light = prompt("Tell me color of the traffic lights!!!");
+if (light === "green") {
+    if (isRoadClear) {
+        document.write ("GO!");
+    }
+    else {
+        document.write ( "Wait, when cars will gone!!!");
+    }
+}
+if (light === "yellow") {
+    if (isRoadClear) {
+        document.write ("Wait!");
+    }
+    else {
+        document.write ( "Still wait!");
+    }
+}
+if (light === "red") {
+    if (isRoadClear) {
+        document.write ("STOP!!!");
+    }
+    else {
+        document.write ( "STOP!!!");
+    }
+}
+if (light === "broked") {
+    document.write ("You can do whatever you want!");
+}
+
+
+
+
+
+
+
 
 
